4df6b9782eb6dce11a77f7c68835b21666abd6c6
benkurzion <benkurzion@gmail.com>
2024-02-17 15:07:33 -0500

    complete but bugged up

--- interpreter.rkt
+++ interpreter.rkt
@@ -42,17 +42,19 @@
 (define interpret-declare-helper
   (lambda (name assign state)
     (cond ((eq? 'd assign) (add-to-state name 'd state))
-    (else (add-to-state name (interpret-expression assign) state)))))
+          (else (add-to-state name (interpret-expression assign) state)))))
 
-;;((boolean? expression) expression) ?? ask harold
+
 (define interpret-expression
   (lambda (expression state)
     (cond ((or(eq? 'd expression) (eq? '~ expression)) 'NaN)
           ((number? expression) expression)
-          ((list? expression) (interpret-exp-helper expression state))
+          ((boolean? expression) expression)
+          ((and (list? expression) (is-condition? expression)) (interpret-boolean expression state))
+          ((list? expression) (interpret-int expression state))
           (else (get-value expression state)))));is a variable
 
-(define interpret-exp-helper
+(define interpret-int
   (lambda (expression state)
     (cond
       ((eq? (leftoperand expression) 'NaN) 'NaN)
@@ -75,12 +77,43 @@
 
 (define interpret-return
   (lambda (return state)
-    (list (car state) (cadr state) (interpret-expression return state)))
+    (list (car state) (cadr state) (interpret-expression return state))))
      
 
 (define interpret-if
   (lambda (statement state)
-    1))
+    (cond ((eq? 'NaN (interpret-boolean (cadr statement) state)) 'NaN)
+          ((interpret-boolean (cadr statement) state) (interpret-statement (caddr statement)))
+          ((not (null? (cadddr statement))) (interpret-statement (cadddr statement))))))
+
+
+;; <condition> <op> <condition> 
+;; <op> <condition>
+;; <boolean> --> true | false | <expression> <op> <expression>
+(define interpret-boolean
+  (lambda (condition state)
+    (cond
+      ((eq? condition 'true) #t)
+      ((eq? condition 'false) #f)
+      ((eq? (leftoperand expression) 'NaN) 'NaN)
+      ((eq? (operator expression) '!) (not (interpret-expression (leftoperand expression))))
+      ((eq? (rightoperand expression) 'NaN) 'NaN)
+      ((eq? (operator expression) '||) (or (interpret-expression (leftoperand expression)) (interpret-expression (rightoperand expression))))
+      ((eq? (operator expression) '&&) (and (interpret-expression (leftoperand expression)) (interpret-expression (rightoperand expression))))
+      ((eq? (operator expression) '==) (eq? (interpret-expression (leftoperand expression)) (interpret-expression (rightoperand expression))))
+      ((eq? (operator expression) '!=) (not (eq? (interpret-expression (leftoperand expression)) (interpret-expression (rightoperand expression)))))
+      ((eq? (operator expression) '<)  (< (interpret-expression (leftoperand expression)) (interpret-expression (rightoperand expression))))
+      ((eq? (operator expression) '>)  (> (interpret-expression (leftoperand expression)) (interpret-expression (rightoperand expression))))
+      ((eq? (operator expression) '<=)  (<= (interpret-expression (leftoperand expression)) (interpret-expression (rightoperand expression))))
+      ((eq? (operator expression) '>=)  (>= (interpret-expression (leftoperand expression)) (interpret-expression (rightoperand expression))))
+      (else 'NaN))))
+
+
+
+(define interpret-while
+  (lambda (statement state)
+    (cond ((interpret-expression (cadr statement) state) (interpret-while statement (interpret-statement (caddr statement) state)))
+          (else state))))
 
 ;; Gets the value of a variable given the current state
 (define get-value
@@ -99,6 +132,21 @@
 (define add-to-state
   (lambda (name value state)
     (list (cons name (car state)) (cons value (cadr state)) (caddr state))))
-                                    
-                
+
+
+(define is-condition?
+  (lambda (statement)
+    (cond ((eq? statement 'true) #t)
+          ((eq? statement 'false) #t)
+          ((boolean? statement) #t)
+          ((eq? (car statement) '==) #t)
+          ((eq? (car statement) '!=) #t)
+          ((eq? (car statement) '<) #t)
+          ((eq? (car statement) '>) #t)
+          ((eq? (car statement) '<=) #t)
+          ((eq? (car statement) '>=) #t)
+          ((eq? (car statement) '&&) #t)
+          ((eq? (car statement) '||) #t)
+          ((eq? (car statement) '!) #t)
+          (else #f))))
            
